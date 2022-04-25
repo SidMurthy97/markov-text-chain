896c0e3c8144829bce2cce9c99e3d0079944f990
Sid Murthy <murthy.siddhant@googlemail.com>
2022-04-25 20:47:24 +0100

    added skeletons for future tests

--- tests/MarkovChain_test.py
+++ tests/MarkovChain_test.py
@@ -1,6 +1,12 @@
 from src.markovchain import MarkovChain
 import re
-def test_chain():
+
+'''TODO: Write tests:
+- what happens when input is empty?
+- Check probabilities are correct assigned 
+- test cases on the actual chain, do the words link correctly?'''
+
+def test_make_chain():
     '''This tests is the chain is generated correctly for perfect input
     with capitalisation and period'''
     input_string = "The quick brown fox jumps over the lazy dog."
@@ -10,3 +16,9 @@ def test_chain():
     mc.generate_output()
 
     assert list(seen_words.keys()) == expected_output
+
+def test_adding_states():
+    pass
+
+def test_computing_probabilities():
+    pass
