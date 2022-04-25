1deea583b557dfb7ca45b659bc184dee2fa0435e
Sid Murthy <murthy.siddhant@googlemail.com>
2022-04-25 20:42:47 +0100

    added test to check correct chain generation

--- tests/MarkovChain_test.py
+++ tests/MarkovChain_test.py
@@ -1,12 +1,12 @@
 from src.markovchain import MarkovChain
-
+import re
 def test_chain():
     '''This tests is the chain is generated correctly for perfect input
     with capitalisation and period'''
     input_string = "The quick brown fox jumps over the lazy dog."
-
+    expected_output = re.findall(r"[\w']+|[.,!?;]", input_string)
     mc = MarkovChain(input_string)
     seen_words = mc.make_chain()
     mc.generate_output()
 
-    assert True == True
+    assert list(seen_words.keys()) == expected_output
